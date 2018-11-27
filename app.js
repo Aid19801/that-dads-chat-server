c45458efa52629612a46742305fa76905d5dff19
Adrian Thompson <adrian.thompson@sky.uk>
2018-11-27 20:09:10 +0000

    gitignore & pushing to heroku

--- app.js
+++ app.js
@@ -9,15 +9,11 @@ const MongoClient = require('mongodb').MongoClient;
 const { DATABASE_USERNAME, DB_PASSWORD, DB_ML_USER } = process.env;
 const MONGO_URL = `mongodb://${DATABASE_USERNAME}:${DB_PASSWORD}@ds243931.mlab.com:43931/${DB_ML_USER}`;
 
-console.log('DB_USERNAME', DATABASE_USERNAME);
-console.log('DB_PASSWORD', DB_PASSWORD);
-console.log('DB_ML_USER', DB_ML_USER);
-
 server.listen(process.env.PORT || 4001, () => console.log(`Heroku: ${process.env.PORT} | LocalHost: 4001`));
 
 websocket.on('connection', (socket) => {
-  console.log('A client just joined on', socket.id);
-
+  
+  console.log('A client just joined on...', socket.id);
 
   setInterval(async () => {
     try {
